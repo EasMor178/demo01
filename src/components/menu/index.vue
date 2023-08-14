44360f19af5b6450c9ffc0f4ac77d6e05e847a50
LiuLi <2422616874@qq.com>
2023-08-14 17:26:10 +0800

    8.14Update

--- src/components/menu/index.vue
+++ src/components/menu/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-menu default-active="1" class="el-menu-vertical-demo" active-text-color="#ffd04b" background-color="#576576"
+    <el-menu default-active="1" class="el-menu-vertical-demo" active-text-color="#ffd04b" background-color="rgb(72,82,92)"
         :collapse="isCollapse" text-color="#fff">
         <div class="top">
             <div>
@@ -31,12 +31,31 @@
                 <template #title>审批管理</template>
             </el-menu-item>
         </div>
-        <div class="bottom">
-            <div class="button " @click=disCollapse>
+        <div>
+            <el-menu-item index="5" v-show="isCollapse">
+                <el-icon>
+                    <setting />
+                </el-icon>
+                <template #title>管理后台</template>
+            </el-menu-item>
+            <el-menu-item index="6" @click=disCollapse v-show="isCollapse">
+                <el-icon>
+                    <Fold />
+                </el-icon>
+                <template #title>隐藏</template>
+            </el-menu-item>
+        </div>
+        <div class="bottom" v-show="!isCollapse">
+            <a href="#">
+                <setting style="width: 20px; color: white;" />
+                <span style="color: white;">管理后台</span>
+            </a>
+            <span class="button " @click=disCollapse v-show="!isCollapse">
                 <!-- <el-icon> -->
-                <icon-menu style="width: 20px; color: white;" />
+                <!-- <Expand v-show="isCollapse" style="width: 20px; color: white;" /> -->
+                <Fold style="width: 20px; color: white;" />
                 <!-- </el-icon> -->
-            </div>
+            </span>
         </div>
     </el-menu>
 </template>
@@ -47,7 +66,7 @@ import {
     Menu as IconMenu,
     SuitcaseLine,
     Setting,
-    // Fold,
+    Fold,
     // Expand
 } from '@element-plus/icons-vue';
 
@@ -75,6 +94,8 @@ const goOther02 = () => {
 
 <style scoped lang="scss">
 .el-menu {
+    display: flex;
+    flex-direction: column;
 
     height: 100vh;
 
@@ -104,21 +125,38 @@ const goOther02 = () => {
 
     }
 
-    .bottom {
+    .item {
+        flex: 1;
+    }
 
+    .bottom {
+        padding: 20px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        height: 56px;
+        border-top: 1px solid rgb(118, 118, 118);
+
+        a {
+            display: flex;
+            // max-height: 0px;
+            white-space: nowrap;
+            align-items: center;
+            overflow: hidden;
+        }
 
         .button {
             height: 30px;
             width: 30px;
-            float: right;
-            margin-right: 15px;
+            // float: right;
+            // margin: 10px 20px;
             display: flex;
             align-items: center;
             justify-content: center;
             border-radius: 5px;
 
             &:hover {
-                background-color: #ccc;
+                background-color: #3f3f3f;
             }
         }
     }
@@ -127,7 +165,7 @@ const goOther02 = () => {
 
 
 .el-menu-vertical-demo:not(.el-menu--collapse) {
-    width: 180px;
+    width: 200px;
     flex-shrink: 0;
 }
 </style>
