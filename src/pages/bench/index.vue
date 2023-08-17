15b838d19a85c4474129c896e485c7d70d739f99
LiuLi <2422616874@qq.com>
2023-08-17 11:13:12 +0800

    8.16Update

--- src/pages/bench/index.vue
+++ src/pages/bench/index.vue
@@ -1,44 +1,77 @@
 <template>
-  <div class="parent">
-    <Vue3DraggableResizable
-      v-if="modules[0].isActive"
-      class="module-class"
-      :parent="true"
-      :x="modules[0].x"
-      :y="modules[0].y"
-      :h="modules[0].h"
-      :w="modules[0].w"
-      :z="modules[0].z"
+  <div class="big-screen">
+    <div
+      ref="screenContainer"
+      class="big-screen-container"
+      :style="{
+        width: '1000px',
+        height: '500px',
+        'background-size': `${moduleGridWidth}px ${moduleGridHeight}px`,
+      }"
     >
-      <div class="box">
-        <span>0000000</span>
-      </div>
-    </Vue3DraggableResizable>
-    <Vue3DraggableResizable
-      v-if="modules[0].isActive"
-      class="module-class"
-      :parent="true"
-      :x="modules[0].x"
-      :y="modules[0].y"
-      :h="modules[0].h"
-      :w="modules[0].w"
-      :z="modules[0].z"
-    >
-      <div class="box">
-        <span>0000000</span>
-      </div>
-    </Vue3DraggableResizable>
+      <VueDraggableResizable
+        v-if="modules[0].isActive"
+        class="module-class"
+        :parent="true"
+        :grid="[moduleGridWidth, moduleGridHeight]"
+        :x="modules[0].x"
+        :y="modules[0].y"
+        :h="modules[0].h"
+        :w="modules[0].w"
+        :z="modules[0].z"
+        @dragging="onDrag"
+        @resizing="onResize"
+        @activated="onActivated(0)"
+      >
+        <span
+          :style="{
+            'text-align': 'center',
+            'line-height': `${modules[0].h}px`,
+          }"
+          >{{ 1 }}</span
+        >
+      </VueDraggableResizable>
+      <VueDraggableResizable
+        v-if="modules[1].isActive"
+        class="module-class"
+        :parent="true"
+        :grid="[moduleGridWidth, moduleGridHeight]"
+        :x="modules[1].x"
+        :y="modules[1].y"
+        :h="modules[1].h"
+        :w="modules[1].w"
+        :z="modules[1].z"
+        @dragging="onDrag"
+        @resizing="onResize"
+        @activated="onActivated(1)"
+      >
+        <span
+          :style="{
+            'text-align': 'center',
+            'line-height': `${modules[1].h}px`,
+          }"
+          >{{ 2 }}</span
+        >
+      </VueDraggableResizable>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import Vue3DraggableResizable from 'vue3-draggable-resizable';
+import VueDraggableResizable from 'vue-draggable-resizable/src/components/vue-draggable-resizable.vue';
+import 'vue-draggable-resizable/dist/VueDraggableResizable.css';
 //default styles
 // import 'vue3-draggable-resizable/dist/Vue3DraggableResizable.css'
 // console.log('1234')
 
-// const moduleGridWidth = 0, // 一格的宽度
-//     moduleGridHeight = 0 // 一格的长度
+let moduleGridWidth = 100;
+let moduleGridHeight = 100;
+let activeIndex: 0;
+// let screenWidth: 0; // 屏幕设置的宽度(画板的宽度)
+// let screenHeight: 0; // 屏幕设置的高度
+// let firstScreenHeight: 100;
+// let userScreenWidth: 100;
+// let userScreenHeight: 10;
 const modules = [
   {
     index: 1,
@@ -51,27 +84,38 @@ const modules = [
     moduleName: '',
     fixTitle: '',
   },
+  {
+    index: 2,
+    isActive: true,
+    x: 0,
+    y: 0,
+    h: 200,
+    w: 200,
+    z: 12,
+    moduleName: '',
+    fixTitle: '',
+  },
 ];
-// function onResize(left, top, width, height) {
-//     this.modules[this.activeIndex].x = left
-//     this.modules[this.activeIndex].y = top
-//     this.modules[this.activeIndex].w = width
-//     this.modules[this.activeIndex].h = height
-// }
-// function onDrag(left, top) {
-//     this.modules[this.activeIndex].x = left
-//     this.modules[this.activeIndex].y = top
-// }
-// function onActivated(index) {
-//     this.activeIndex = index
-//     // if (index < 3) {
-//     //     this.$refs.singleTable.bodyWrapper.scrollTop = 0
-//     // } else {
-//     //     this.$refs.singleTable.bodyWrapper.scrollTop = (index - 1) * 57
-//     // }
-//     // // 标记被选中的行
-//     // this.$refs.singleTable.setCurrentRow(this.modules[index])
-// }
+function onResize(left, top, width, height) {
+  modules[activeIndex].x = left;
+  modules[activeIndex].y = top;
+  modules[activeIndex].w = width;
+  modules[activeIndex].h = height;
+}
+function onDrag(left, top) {
+  modules[activeIndex].x = left;
+  modules[activeIndex].y = top;
+}
+function onActivated(index) {
+  activeIndex = index;
+  // if (index < 3) {
+  //     this.$refs.singleTable.bodyWrapper.scrollTop = 0
+  // } else {
+  //     this.$refs.singleTable.bodyWrapper.scrollTop = (index - 1) * 57
+  // }
+  // // 标记被选中的行
+  // this.$refs.singleTable.setCurrentRow(this.modules[index])
+}
 </script>
 <style scoped lang="scss">
 .parent {
@@ -80,11 +124,75 @@ const modules = [
   position: absolute;
   border: 1px solid #000;
   user-select: none;
+}
 
-  .box {
+.moduleSpan {
+  align-self: center;
+  justify-self: center;
+}
+
+.big-screen {
+  position: relative;
+  display: grid;
+  padding: 1em;
+  height: calc(100vh - 130px);
+  grid-template-columns: 25% 25% 25% 25%;
+  grid-template-rows: repeat(10, 10%);
+
+  .big-screen-container {
+    display: block;
+    justify-self: center;
+    background-color: rgb(121, 121, 121);
+    background: linear-gradient(-90deg, rgba(0, 0, 0, 0.1) 1px, transparent 1px),
+      linear-gradient(rgba(0, 0, 0, 0.1) 1px, transparent 1px);
+    border: 1px solid blue;
+    grid-column-start: span 4;
+    grid-row-start: span 6;
+
+    .module-class {
+      display: inline-grid;
+      background-color: #808080;
+      border: 1px solid #000;
+      color: #fff;
+      font-size: 20px;
+      font-weight: bold;
+      font-family: YouYuan;
+    }
+  }
+
+  .setting-table {
+    grid-column-start: 1;
+    grid-column-end: 5;
+    grid-row-start: 8;
+    grid-row-end: 11;
+  }
+
+  .module-settings {
+    display: flex;
+    grid-column-start: span 4;
+    justify-content: space-between;
+    padding-top: 1em;
+    font-size: 20px;
+  }
+
+  .screen-button {
+    grid-column-start: 4;
+    padding-top: 1em;
+  }
+
+  .dl-icon {
+    font-size: 20px;
+  }
+
+  &.screenfull {
+    position: fixed;
+    top: 0;
+    left: 0;
     width: 100%;
     height: 100%;
-    background: #ffcaa4;
+    z-index: 2000;
+    background-color: #f0f2f5;
+    overflow: auto;
   }
 }
 </style>
